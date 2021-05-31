8aaee961a45b58a9f069c24aa2a444657fc38cfb
chandu078 <chandudyavanapelli03@gmail.com>
2021-05-31 21:05:05 +0200

    sm8350-common: Fixup PowerShare - Missing on 6d37409.
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- powershare/PowerShare.cpp
+++ powershare/PowerShare.cpp
@@ -47,8 +47,7 @@ static T get(const std::string& path, const T& def) {
 }
 
 Return<bool> PowerShare::isEnabled() {
-    const auto value = get<std::string>(WIRELESS_TX_ENABLE_PATH, "");
-    return !(value == "disable" || value == "0");
+    return get<int>(WIRELESS_TX_ENABLE_PATH, 0) >= 1;
 }
 
 Return<bool> PowerShare::setEnabled(bool enable) {
