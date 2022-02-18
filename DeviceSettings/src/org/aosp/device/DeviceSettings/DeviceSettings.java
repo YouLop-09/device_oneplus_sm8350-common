37f8617d29d4aa12c1aa2063d535719b3be6574b
YouLop-09 <omar.americanista1@gmail.com>
2022-02-18 10:28:35 -0600

    sm8350-common: DeviceSettings: Drop 5g modes switcher
    - Not working with bsp blobs
    - Also drop persist.vendor.radio.5g_mode_pref (It's prevents 4g switching in bsp blobs)
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- DeviceSettings/src/org/aosp/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/org/aosp/device/DeviceSettings/DeviceSettings.java
@@ -18,17 +18,12 @@
 package org.aosp.device.DeviceSettings;
 
 import android.app.ActivityManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.provider.Settings;
-import android.telephony.SubscriptionManager;
-import android.widget.Toast;
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
@@ -36,8 +31,6 @@ import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference;
 import androidx.preference.TwoStatePreference;
-
-import com.qualcomm.qcrilmsgtunnel.IQcrilMsgTunnel;
 
 import org.aosp.device.DeviceSettings.Doze.DozeSettingsActivity;
 import org.aosp.device.DeviceSettings.ModeSwitch.*;
@@ -60,7 +53,6 @@ public class DeviceSettings extends PreferenceFragment
     public static final String KEY_FPS_INFO_POSITION = "fps_info_position";
     public static final String KEY_FPS_INFO_COLOR = "fps_info_color";
     public static final String KEY_FPS_INFO_TEXT_SIZE = "fps_info_text_size";
-    public static final String KEY_NR_MODE_SWITCHER = "nr_mode_switcher";
     public static final String KEY_GAME_SWITCH = "game_mode";
     public static final String KEY_EDGE_TOUCH = "edge_touch";
     public static final String KEY_VIBSTRENGTH = "vib_strength";
@@ -71,7 +63,6 @@ public class DeviceSettings extends PreferenceFragment
     private static final String FILE_LEVEL = "/sys/devices/platform/soc/88c000.i2c/i2c-10/10-005a/leds/vibrator/level";
     public static final String DEFAULT = "3";
 
-    private Protocol mProtocol;
     private DolbySwitch mDolbySwitch;
     private Preference mDozeSettings;
 
@@ -94,22 +85,6 @@ public class DeviceSettings extends PreferenceFragment
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getContext());
         addPreferencesFromResource(R.xml.main);
-
-        Intent mIntent = new Intent();
-        mIntent.setClassName("com.qualcomm.qcrilmsgtunnel", "com.qualcomm.qcrilmsgtunnel.QcrilMsgTunnelService");
-        getContext().bindService(mIntent, new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                IQcrilMsgTunnel tunnel = IQcrilMsgTunnel.Stub.asInterface(service);
-                if (tunnel != null)
-                    mProtocol = new Protocol(tunnel);
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-                mProtocol = null;
-            }
-        }, getContext().BIND_AUTO_CREATE);
 
         mMuteMedia = (TwoStatePreference) findPreference(KEY_MUTE_MEDIA);
         mMuteMedia.setChecked(PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(DeviceSettings.KEY_MUTE_MEDIA, false));
@@ -153,9 +128,6 @@ public class DeviceSettings extends PreferenceFragment
 
         mFpsInfoTextSizePreference = (CustomSeekBarPreference) findPreference(KEY_FPS_INFO_TEXT_SIZE);
         mFpsInfoTextSizePreference.setOnPreferenceChangeListener(this);
-
-        mNrModeSwitcher = (ListPreference) findPreference(KEY_NR_MODE_SWITCHER);
-        mNrModeSwitcher.setOnPreferenceChangeListener(this);
 
         mGameModeSwitch = (TwoStatePreference) findPreference(KEY_GAME_SWITCH);
         if (GameModeSwitch.isSupported()) {
@@ -255,9 +227,6 @@ public class DeviceSettings extends PreferenceFragment
                     restartFpsInfo(mContext);
                 }
             }
-        } else if (preference == mNrModeSwitcher) {
-            int mode = Integer.parseInt(newValue.toString());
-            return setNrModeChecked(mode);
         } else if (preference == mVibratorStrengthPreference) {
     	    int value = Integer.parseInt(newValue.toString());
             SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
@@ -299,30 +268,5 @@ public class DeviceSettings extends PreferenceFragment
         Intent fpsinfo = new Intent(context, FPSInfoService.class);
         context.stopService(fpsinfo);
         context.startService(fpsinfo);
-    }
-
-    private boolean setNrModeChecked(int mode) {
-        switch (mode) {
-            case 0:
-                return setNrModeChecked(Protocol.NR_5G_DISABLE_MODE_TYPE.NAS_NR5G_DISABLE_MODE_SA);
-            case 1:
-                return setNrModeChecked(Protocol.NR_5G_DISABLE_MODE_TYPE.NAS_NR5G_DISABLE_MODE_NSA);
-            default:
-                return setNrModeChecked(Protocol.NR_5G_DISABLE_MODE_TYPE.NAS_NR5G_DISABLE_MODE_NONE);
-        }
-    }
-
-    private boolean setNrModeChecked(Protocol.NR_5G_DISABLE_MODE_TYPE mode) {
-        if (mProtocol == null) {
-            Toast.makeText(getContext(), R.string.service_not_ready, Toast.LENGTH_LONG).show();
-            return false;
-        }
-        int index = SubscriptionManager.getSlotIndex(SubscriptionManager.getDefaultDataSubscriptionId());
-        if (index == SubscriptionManager.INVALID_SIM_SLOT_INDEX) {
-            Toast.makeText(getContext(), R.string.unavailable_sim_slot, Toast.LENGTH_LONG).show();
-            return false;
-        }
-        new Thread(() -> mProtocol.setNrMode(index, mode)).start();
-        return true;
     }
 }
